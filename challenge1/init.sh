595f5accb02b653f638048cbb06f0d3e0452f511
Marcela Bonell <marcelabonell@gmail.com>
2016-09-02 12:33:45 -0500

    Fix init for challlenge1

--- challenge1/init.sh
+++ challenge1/init.sh
@@ -2,5 +2,5 @@
 sudo apt-get update
 sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
 
-wget https://raw.githubusercontent.com/openstack-hackathon/training-shade-cucea/master/challenge/my_landing_page.html
+wget https://raw.githubusercontent.com/openstack-hackathon/training-shade-cucea/master/challenge1/my-landing-page.html
 sudo cp shade-demo.html /var/www/html/index.html
